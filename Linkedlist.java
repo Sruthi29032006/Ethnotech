ad888137b53fdcc01ae0785c3bd958600302b99c
sruthi <poreddysruthi2006@gmail.com>
2026-06-11 12:38:50 +0530

    first commit

---
+++ Linkedlist.java
@@ -0,0 +1,17 @@
+
+import java.util.LinkedList;
+
+class Linkedlist{
+    public static void main(String args[]){
+        LinkedList<String> ss=new LinkedList<>();
+        ss.add("chitti");
+        ss.add("Usha");
+        ss.add("tej");
+        ss.add("varsh");
+        ss.add("laari");
+        ss.add("palls");
+        ss.add("venns");
+        ss.add("akki");
+        System.out.println(ss);
+    }
+}
